09b8e3fe4367dbe51735953cb2215fa6f8583c6b
mayuri <mayurimaheshwari760@gmail.com>
2024-12-11 18:12:00 +0530

    new line

---
+++ Basic/File3.java
@@ -0,0 +1,11 @@
+public class File3 {
+    public static void main(String[] args) {
+        System.out.println("Dosa");
+         System.out.println("Momo");
+          System.out.println("Pasta");
+           System.out.println("Paneer 65");
+            System.out.println("Corn & Cheese balls");
+    }
+}
+
+
