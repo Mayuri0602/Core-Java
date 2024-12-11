b79e46f9d459a64a3744e152da2209fde1ed906d
mayuri <mayurimaheshwari760@gmail.com>
2024-12-11 19:09:43 +0530

    cities new line

---
+++ Basic/File4.java
@@ -0,0 +1,9 @@
+public class File4 {
+    public static void main(String[] args) {
+        System.out.println("Bangalore");
+         System.out.println("Mumbai");
+          System.out.println("Delhi");
+           System.out.println("Jaipur");
+            System.out.println("Surat");
+    }
+}
