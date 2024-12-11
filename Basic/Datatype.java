334f814373a1764a9d309d1a8885f78d8a825009
mayuri <mayurimaheshwari760@gmail.com>
2024-12-12 00:40:10 +0530

    primitive data types

---
+++ Basic/Datatype.java
@@ -0,0 +1,62 @@
+// public class Datatype {
+//    public static void main(String[] args) {
+//        boolean a= true;
+//        System.out.println("boolean : " +a);
+//    }
+// }
+
+
+// public class Datatype {
+//     public static void main(String[] args) {
+//         char c= 'o';
+//         System.out.println("char : " +c);
+//     }
+// }
+
+
+// public class Datatype {
+//     public static void main(String[] args) {
+//         byte b= 123;
+//         System.out.println("byte : " +b);
+//     }
+// }
+
+
+// public class Datatype {
+//     public static void main(String[] args) {
+//         short s= 345;
+//         System.out.println("short : " +s);
+//     }
+// }
+
+
+// public class Datatype {
+//     public static void main(String[] args) {
+//         int i= 45;
+//         System.out.println("int : " +i);
+//     }
+// }
+
+
+// public class Datatype {
+//     public static void main(String[] args) {
+//         long l= 9845;
+//         System.out.println("long : " +l);
+//     }
+// }
+
+
+public class Datatype {
+    public static void main(String[] args) {
+        float f= 98.45f;
+        System.out.println("float : " +f);
+    }
+}
+
+
+// public class Datatype {
+//     public static void main(String[] args) {
+//         double d= 6.06;
+//         System.out.println("double : " +d);
+//     }
+// }
