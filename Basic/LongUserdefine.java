866f020fe24eb05b3d9174526e9f23fbb638c426
mayuri <mayurimaheshwari760@gmail.com>
2024-12-12 17:11:48 +0530

    datatype long user define method

---
+++ Basic/LongUserdefine.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class LongUserdefine {
+ public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the value: ");
+    long a=sc.nextLong();
+    System.out.println("value entered is: " +a);
+ }   
+}
