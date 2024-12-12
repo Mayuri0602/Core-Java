d0a513b8699c9ec4331ff89cf2c7abd81ac8c9e4
mayuri <mayurimaheshwari760@gmail.com>
2024-12-12 09:36:23 +0530

    float practice ques.

---
+++ Basic/Float.java
@@ -0,0 +1,8 @@
+public class Float {
+    public static void main(String[] args) {
+        float a=1.2f, b=6.7f, c=4.5f, d=2.0f, e;
+        e=a*b-c/d;
+        System.out.println("result: " +e);
+    }
+    
+}
