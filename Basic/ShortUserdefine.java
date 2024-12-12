a9089c3cc37adb7e748bdc81a123e3a5230f0ccd
mayuri <mayurimaheshwari760@gmail.com>
2024-12-12 16:42:04 +0530

    short data type user define method

---
+++ Basic/ShortUserdefine.java
@@ -0,0 +1,14 @@
+
+
+import java.util.Scanner;
+
+public class ShortUserdefine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value: ");
+        short a = sc.nextShort();
+        System.out.println("Entered value is: " + a);
+    }    
+}
+
+
