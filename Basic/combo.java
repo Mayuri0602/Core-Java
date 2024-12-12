39d6a8ebde614bb436432cbd9b0e4d2f5fb92073
mayuri <mayurimaheshwari760@gmail.com>
2024-12-12 09:22:45 +0530

    arithmatic ques.

---
+++ Basic/combo.java
@@ -0,0 +1,7 @@
+public class combo {
+    public static void main(String[] args) {
+        int a=56, b=34, c=12, d=2, e=13, f;
+        f=a*b+c/d-e;
+        System.out.println("Result:" +f);
+    }
+}
