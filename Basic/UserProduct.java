38862e33274873c0b9c17b439898bc057afb10ef
mayuri <mayurimaheshwari760@gmail.com>
2024-12-13 00:13:42 +0530

    multiply using user input method

---
+++ Basic/UserProduct.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class UserProduct {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of a : ");
+        int a=sc.nextInt();
+        System.out.println("Enter the value of b : ");
+        int b=sc.nextInt();
+        int c=a*b;
+        System.out.println("Value of c is equal to : " + c);
+    }
+}
+    
