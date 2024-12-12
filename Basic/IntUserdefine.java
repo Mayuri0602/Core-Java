5837fde938708535b7cd6a15c32b61764eec1451
mayuri <mayurimaheshwari760@gmail.com>
2024-12-12 15:18:11 +0530

    predefine method

---
+++ Basic/IntUserdefine.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class IntUserdefine {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a=sc.nextInt();
+    System.out.println("The value of a is" +a);   
+    }
+}
