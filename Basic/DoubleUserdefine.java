578f59cbfc342d53319fee5d4062cf789d5e15ba
mayuri <mayurimaheshwari760@gmail.com>
2024-12-12 16:18:12 +0530

    user define method for double

---
+++ Basic/DoubleUserdefine.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class DoubleUserdefine {
+ public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    double d=sc.nextDouble();
+    System.out.println("value : " +d);
+ }   
+}
