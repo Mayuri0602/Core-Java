7aafd5cfc2be2b03ec7e410254c46c9ad10c0c9f
mayuri <mayurimaheshwari760@gmail.com>
2024-12-12 16:08:07 +0530

    byte user define method

---
+++ Basic/ByteUserdefine.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ByteUserdefine {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        byte b=sc.nextByte();
+        System.out.println("Enter the value of b : " + b);
+    }
+    
+}
