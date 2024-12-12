7c60eebc1a2b667c9a340a55d6eb6d4aeb750ac9
mayuri <mayurimaheshwari760@gmail.com>
2024-12-12 13:04:56 +0530

    preference over int & float

---
+++ Basic/Arithmatic2.java
@@ -0,0 +1,8 @@
+public class Arithmatic2 {
+    public static void main(String[] args) {
+      float a=8.9f, b=1.67f, c=6.21f, d=5.6f, e=7.8f, r;
+      int f=34, g=157;
+      r=a*b-(c-d)+e-f+g;
+      System.out.println("Solution :"+r);  
+    }
+}
