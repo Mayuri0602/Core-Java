adcfcccdecfd26e78c529e7fcd0e0c863af08c73
mayuri <mayurimaheshwari760@gmail.com>
2024-12-12 09:42:43 +0530

    practice ques.

---
+++ Basic/Double.java
@@ -0,0 +1,7 @@
+public class Double {
+    public static void main(String[] args) {
+        double a=1.6, b=12, c=2, d=2, e=7.8, f=1.5,r;
+r= a*(b-c)/d+e-f;
+System.out.println("result:" +r);
+    }
+}
