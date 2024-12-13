f0967bd0b5db561098726c8e26f9426adcc33ea0
mayuri <mayurimaheshwari760@gmail.com>
2024-12-13 12:08:02 +0530

    user input string of one word

---
+++ Basic/Stringuser1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Stringuser1 {
+public static void main (String[] args) {
+    Scanner sc=new Scanner (System.in);
+    System.out.println("enter a word");
+    String s=sc.next();
+    System.out.println(s);
+}    
+}
