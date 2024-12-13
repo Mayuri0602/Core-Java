12d60ba057d44d6183db3b9cc31f6b5181335015
mayuri <mayurimaheshwari760@gmail.com>
2024-12-13 23:58:47 +0530

    add new folder

---
+++ Basic/Primitivedatatype.java
@@ -0,0 +1,13 @@
+public class Primitivedatatype {
+    public static void main(String[] args) {
+        int a=76;
+        float b=56.89f;
+        char c='M';
+        short d=231;
+        long e=6012002;
+        double f=7.3214;
+        byte g=7;
+        boolean h=false;
+        System.out.println("int:" +a+ "float:" +b+ "char:" +c+ "short:" +d+ "long:" +e+ "double:" +f+ "byte:" +g+ "boolean:" +h);
+    }
+}
