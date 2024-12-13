479652a09a3aa9b0709033fd3633b153c14bdcd3
mayuri <mayurimaheshwari760@gmail.com>
2024-12-13 12:10:59 +0530

    user input string for more than a word

---
+++ Basic/Stringuser2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Stringuser2 {
+public static void main (String[] args) {
+    Scanner sc=new Scanner (System.in);
+    System.out.println("enter a paragraph");
+    String s=sc.nextLine();
+    System.out.println(s);
+}    
+}
