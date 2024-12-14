7cae01191b1214a5a0bb3e5a7a4c835de01fc682
mayuri <mayurimaheshwari760@gmail.com>
2024-12-14 10:22:12 +0530

    two number max

---
+++ Decision_Making/Num2Max.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Num2Max {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter first number");
+        int a=sc.nextInt();
+        System.out.println("enter second number");
+        int b=sc.nextInt();
+        if(a>b){
+            System.out.println("a is maximum");
+        }else if(b>a)
+        {
+                System.out.println("b is maximum");
+            }
+        }
+    }
+    
+
