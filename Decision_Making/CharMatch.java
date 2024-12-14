a1e3b011c0dab2648a2eef62899b0c46a18932c0
mayuri <mayurimaheshwari760@gmail.com>
2024-12-14 17:39:33 +0530

    alphabet & word

---
+++ Decision_Making/CharMatch.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class CharMatch {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the character");
+        char ch=sc.next().charAt(0);
+        if (ch=='a'){
+            System.out.println("apple");
+        } 
+        else if (ch=='b'){
+        System.out.println("ball");}
+        else if (ch=='c'){
+            System.out.println("cat");
+        }
+        else{
+            System.out.println("input is not matched");
+        }
+    }
+    
+}
