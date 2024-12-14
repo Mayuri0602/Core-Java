0cfd6cc85cf72a86c06850443b66657262cfa12f
mayuri <mayurimaheshwari760@gmail.com>
2024-12-14 19:18:21 +0530

    arithmatic operators

---
+++ 14dec_test/Operators.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Operators {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter num1 : ");
+    int num1=sc.nextInt( );
+    System.out.println("enter num2 :");
+    int num2=sc.nextInt( );
+    int add=num1+num2;
+    int sub=num1-num2;
+    int product=num1*num2;
+    double division=num1/num2;
+    System.out.println("addition of "+num1+" and "+num2+" is : " +add);
+    System.out.println("subtraction of "+num1+" and "+num2+" is : " +sub);
+    System.out.println("product of "+num1+" and "+num2+" is : " +product) ;
+    System.out.println("division of "+num1+" and "+num2+" is : " +division);
+    }
+    
+}
