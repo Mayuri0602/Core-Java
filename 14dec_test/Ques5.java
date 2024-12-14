05882379dc9400e1d639881c36def48ae0bae4c7
mayuri <mayurimaheshwari760@gmail.com>
2024-12-14 19:34:08 +0530

    even odd num

---
+++ 14dec_test/Ques5.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class Ques5 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("Even number");
+        }
+        else if(n%2==1){
+            System.out.println("Odd number");
+        }
+    }
+}
