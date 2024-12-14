c4b3397993fbe8415dea9112b16bc3fae2cb578c
mayuri <mayurimaheshwari760@gmail.com>
2024-12-14 10:08:11 +0530

    one num max

---
+++ Decision_Making/NumberMax.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class NumberMax{
+   public static void main(String [ ]args){
+      Scanner sc=new Scanner(System.in);
+    System.out.println("Enter one number");
+   int a=sc.nextInt();
+       if(a>5){
+       System.out.print("a is greater than 5");
+      } 
+      else{
+         System.out.print("a is not greater than 5");
+      }
+   }
+}
