1053e2664b26aa5b4c1ee699354b8159cf5da65a
mayuri <mayurimaheshwari760@gmail.com>
2024-12-14 18:50:49 +0530

    user input double datatype

---
+++ 14dec_test/Variable2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Variable2 {
+public static void main(String[] args) {
+    Scanner sc=new Scanner (System.in);
+    System.out.println("enter the number");
+    double n=sc.nextDouble();
+    System.out.println("number:" +n);
+}
+}
