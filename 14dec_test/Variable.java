add61da8f427d3e42cd32e40347632e6832ac763
mayuri <mayurimaheshwari760@gmail.com>
2024-12-14 18:47:48 +0530

    user input declare variable

---
+++ 14dec_test/Variable.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Variable {
+public static void main(String[] args) {
+    Scanner sc=new Scanner (System.in);
+    System.out.println("enter the age");
+    int n=sc.nextInt();
+    System.out.println("age:" +n);
+}
+}
