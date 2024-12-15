300432564baae4be83d2728884bca556e08b05f5
mayuri <mayurimaheshwari760@gmail.com>
2024-12-15 15:59:03 +0530

    pallindrome number

---
+++ 14dec_test/Ques13.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Ques13 {
+ public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num=sc.nextInt();
+        int temp=num;
+        int rem,sum=0;
+        while(num>0){
+            rem=num%10;
+            sum=sum*10+rem;
+            num=num/10;
+        }
+      //  System.out.println("sum:"+sum);
+        if(temp==sum){
+            System.out.println("Palindrome number");
+        }
+        else{
+            System.out.println("Not a Palindrome number");
+        }
+    }
+}
+ 
