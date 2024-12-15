8d088a91742a5e2394dd97e8b58f34ceb6229aef
mayuri <mayurimaheshwari760@gmail.com>
2024-12-15 13:08:16 +0530

    while loop (even num)

---
+++ 14dec_test/Type.java
@@ -0,0 +1,14 @@
+public class Type{
+    public static void main(String[] args){
+        int a=20;
+        int b=30;
+        while(a<=b){
+            if(a%2==0){
+                System.out.println(a);
+            }
+            a++;
+        }
+       
+
+    }
+}
