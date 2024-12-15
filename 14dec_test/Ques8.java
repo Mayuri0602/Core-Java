efcc25d53b0ca6bd3ee6e1c69ec72d53e7f235db
mayuri <mayurimaheshwari760@gmail.com>
2024-12-15 12:18:04 +0530

    score & grade

---
+++ 14dec_test/Ques8.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Ques8 {
+public static void main(String[] args) {
+    Scanner sc=new Scanner (System.in);
+    System.out.println("Enter student's score : ");
+    int s=sc.nextInt();
+    if(s>90){
+        System.out.println("Grade A");
+    }
+   else if(s>80 && s<90){
+        System.out.println("Grade B");
+    }
+    else if(s>70 && s<80){
+        System.out.println("Grade C");
+    }
+    else if(s>60 && s<70){
+        System.out.println("Grade D");
+    }
+    else{
+        System.out.println("Fail");
+    }
+}    
+}
