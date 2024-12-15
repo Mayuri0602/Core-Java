86679f18aa95b75a9080f1858bda2e272b5e1ef5
mayuri <mayurimaheshwari760@gmail.com>
2024-12-15 14:51:07 +0530

    sum using for loop

---
+++ 14dec_test/Ques11.java
@@ -0,0 +1,9 @@
+public class Ques11 {
+    public static void main(String[] args) {
+       int n=100,sum=0;
+       for(int i=1;i<=n;i++){
+        sum+=i;
+       }
+    System.out.println("Sum of natural numbers : " +sum);
+    }
+}
