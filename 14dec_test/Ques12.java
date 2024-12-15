41e348a120502bb01d53f2d53afcee3739fd3647
mayuri <mayurimaheshwari760@gmail.com>
2024-12-15 15:19:59 +0530

    factorial ques using for loop

---
+++ 14dec_test/Ques12.java
@@ -0,0 +1,9 @@
+public class Ques12 {
+    public static void main(String[] args) {
+        int fact=1;
+        for(int i=1;i<=10;i++){
+        fact*=i;
+    }
+    System.out.print("factorial of 10 : " +fact);
+}
+}
