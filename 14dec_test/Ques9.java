13ba15fba847a47f47987233140f1851014b9c0a
mayuri <mayurimaheshwari760@gmail.com>
2024-12-15 13:07:02 +0530

    while loop for even numbers

---
+++ 14dec_test/Ques9.java
@@ -0,0 +1,11 @@
+public class Ques9 {
+    public static void main(String[] args) {
+        int n=20;
+        while(n<30){
+            if (n%2==0){
+                System.out.print("even number :" +n);
+            }
+            n++;
+        }
+    }
+}
