293b8cd0a6436918db0277d093e55bb4201bfa7b
mayuri <mayurimaheshwari760@gmail.com>
2024-12-18 09:04:44 +0530

    even no.s. from 100 to 1

---
+++ loops/Ques4.java
@@ -0,0 +1,13 @@
+public class Ques4 {
+        public static void main(String[] args) {
+            int a=100;
+            while(a>=1){
+                if(a%2==0){
+                    System.out.println(a);
+                }
+                a--;
+            }
+        }
+    }
+     
+
