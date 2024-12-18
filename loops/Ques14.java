47ddca9176ec19b9d799fafbd90e43c1aaff10b2
mayuri <mayurimaheshwari760@gmail.com>
2024-12-18 16:10:06 +0530

    print table using user input

---
+++ loops/Ques14.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ques14 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n=sc.nextInt();
+        int i=1;
+        int table;
+        while(i<=10){
+            table=i*n;
+            System.out.println("Table of +'n' =" +table);
+            i++;
+        }
+    }
+}
