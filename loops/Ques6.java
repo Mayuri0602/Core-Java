1f74062bdf64d599b81e30722946b1742c138cec
mayuri <mayurimaheshwari760@gmail.com>
2024-12-18 09:09:19 +0530

    50 to 500 no.s.

---
+++ loops/Ques6.java
@@ -0,0 +1,10 @@
+public class Ques6 {
+    public static void main(String [ ] args){
+        int a=50;
+        while(a<=500){
+            System.out.println(a);
+            a++;
+        }
+}
+}
+
