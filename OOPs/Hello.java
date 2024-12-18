7810d1e866b8a39715b3fcd5b0afe337df31209f
mayuri <mayurimaheshwari760@gmail.com>
2024-12-18 12:50:28 +0530

    print hello world

---
+++ OOPs/Hello.java
@@ -0,0 +1,11 @@
+class A{
+    void show( ){
+        System.out.println("Hello World");
+}
+}
+public class Hello{
+    public static void main(String[] args) {
+        A h=new A();
+        h.show( );
+    }
+}
