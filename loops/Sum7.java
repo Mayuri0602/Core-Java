bf6d5912ff741cfa0bbf76e24b85a3fcb6573b46
mayuri <mayurimaheshwari760@gmail.com>
2024-12-19 00:24:36 +0530

    user input check prime no. using while loop

---
+++ loops/Sum7.java
@@ -0,0 +1,24 @@
+public class Sum7 {
+        public static void main(String[] args){
+            int num=19;
+            int sum=0;
+            int a=2;
+            while(a<num){
+                if(num%2==0){
+                    sum=1;
+                }
+    
+                a++;
+    
+            }
+            if(sum==0){
+                System.out.print("prime number");
+            }
+            else{
+                System.out.print("not a prime number");
+            }
+    
+        }
+    }
+    
+
