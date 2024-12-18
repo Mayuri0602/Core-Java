3cc53a7b5fe62fe631c78da41f9bcfe82f3f85fa
mayuri <mayurimaheshwari760@gmail.com>
2024-12-18 09:11:53 +0530

    500 to 50

---
+++ loops/Ques7.java
@@ -0,0 +1,9 @@
+public class Ques7 {
+    public static void main(String [ ] args){
+        int a=500;
+        while(a>=5){
+            System.out.print(a+ " ");
+            a--;
+        }
+}
+}
