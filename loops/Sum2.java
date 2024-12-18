ff38ec18c20526574b028c88249016b3b5812da3
mayuri <mayurimaheshwari760@gmail.com>
2024-12-18 21:13:15 +0530

    print pallindrome number using while loop

---
+++ loops/Sum2.java
@@ -0,0 +1,18 @@
+public class Sum2 {
+    public static void main(String[] args) {
+        int n=1321;
+        int temp=n;
+        int rem,sum=0;
+        while(n>0){
+            rem=n%10;
+            sum=sum*10+rem;
+            n=n/10;
+        }
+        if (temp==sum){
+            System.out.println("Pallindrome number");
+        }
+        else{
+            System.out.println("Not a pallindrome number");
+        }
+    }
+}
