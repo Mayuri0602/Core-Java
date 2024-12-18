8421cd767ac5f2438470ebed2231bbba83ba4bba
mayuri <mayurimaheshwari760@gmail.com>
2024-12-18 16:38:13 +0530

    print table of 2 in reverse

---
+++ loops/Ques19.java
@@ -0,0 +1,13 @@
+public class Ques19 {
+    public static void main(String[] args) {
+        
+    int n=2;
+        int table;
+        int i=10;
+        while(i>=1){
+            table=i*n;
+            System.out.println(table);
+            i--;
+        }
+}
+}
