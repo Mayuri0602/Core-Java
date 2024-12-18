398bde85c8bdca2a5e1ed15dd29f2435aff71f89
mayuri <mayurimaheshwari760@gmail.com>
2024-12-18 09:14:09 +0530

    even no.s. from 50 to 1000

---
+++ loops/Ques8.java
@@ -0,0 +1,11 @@
+public class Ques8 {
+    public static void main(String[] args) {
+        int a=50;
+        while(a<=1000){
+            if(a%2==0){
+                System.out.print(a+ " ");
+            }
+            a++;
+        }
+}
+}
