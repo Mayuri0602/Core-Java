1f71fe600c06af1452e4b8b4a15b0c4f3c02beab
mayuri <mayurimaheshwari760@gmail.com>
2024-12-19 17:51:59 +0530

    divisibility by 5 and 11

---
+++ loops_2/Ques14.java
@@ -0,0 +1,13 @@
+public class Ques14 {
+    public static void main(String[] args) {
+     int m=5;
+     int n=11;
+     System.out.print("enter the vale of m:" +m);
+     System.out.println("enter the value of n:" +n);
+        for(int i=1;i<=100;i++){
+            if(i%5==0 && i%11==0){
+                System.out.print(i+" ");
+            }
+}
+    }
+}
