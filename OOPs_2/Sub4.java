6c0ae8b26ac09d797cd5dfbbbdb798163a68f70c
mayuri <mayurimaheshwari760@gmail.com>
2024-12-20 00:25:37 +0530

    string class using return & no argument

---
+++ OOPs_2/Sub4.java
@@ -0,0 +1,12 @@
+class A{
+    String show ( ){
+        String name="Hello Regex";
+        return name;
+    }
+}
+public class Sub4{
+    public static void main(String[] args) {
+        A s=new A( );
+        System.out.println(s.show( ));
+    }
+}
