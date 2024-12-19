04d07295545a2bcfc88d04a699a728babd34dd92
mayuri <mayurimaheshwari760@gmail.com>
2024-12-19 17:18:10 +0530

    100 to 1 odd no. sum

---
+++ loops_2/Ques8.java
@@ -0,0 +1,12 @@
+public class Ques8 {
+    public static void main(String[] args) {
+        int sum=0;
+        for (int i=100;i>=1;i--){
+            if(i%2==1){
+                sum+=i;}
+            }
+                System.out.print(+sum); 
+                
+        
+    }
+}
