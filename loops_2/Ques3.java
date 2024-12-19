cde1253afee42ff7b6afc7f79b1c70f9bbf5c524
mayuri <mayurimaheshwari760@gmail.com>
2024-12-19 09:53:03 +0530

    user input print table using for loop

---
+++ loops_2/Ques3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Ques3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        int n=sc.nextInt( );
+        int table;
+        for(int i=1;i<=10;i++){
+            table=n*i;
+        
+        System.out.println(table);
+        }
+    }
+}
+
+
+
+
