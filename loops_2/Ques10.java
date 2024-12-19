6f2892d3f477a14b4b2d7cf56ae3c74058f0c726
mayuri <mayurimaheshwari760@gmail.com>
2024-12-19 17:21:46 +0530

    odd no. sum using for loop

---
+++ loops_2/Ques10.java
@@ -0,0 +1,12 @@
+public class Ques10 {
+    public static void main(String[] args) {
+        int sum=0;
+        for (int i=1000;i>=500;i--){
+            if(i%2!=0){
+                sum+=i;}
+            }
+                System.out.print(+sum); 
+                
+        
+    }
+}
