15e60fa0384ce1793848fbbded151ee7aaf53948
mayuri <mayurimaheshwari760@gmail.com>
2024-12-19 17:25:24 +0530

    table 2 to 10 using for loop

---
+++ loops_2/Ques11.java
@@ -0,0 +1,12 @@
+public class Ques11 {
+    public static void main(String[] args) {
+        int table=0;
+        for(int i=2;i<=10;i++){
+            for(int j=1;j<=10;j++)
+            {
+                System.out.print(i*j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
