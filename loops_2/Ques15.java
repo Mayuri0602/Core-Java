e10ad6a2827eccbb3ad21ce14e64c0c2446ee0b4
mayuri <mayurimaheshwari760@gmail.com>
2024-12-19 17:57:08 +0530

    table print in reverse

---
+++ loops_2/Ques15.java
@@ -0,0 +1,10 @@
+public class Ques15 {
+    public static void main(String[] args) {
+        int table;
+        int i=2;
+        for(int j=10;j>=1;j--){
+         table=i*j;
+         System.out.println( +table);
+        }
+    }
+}
