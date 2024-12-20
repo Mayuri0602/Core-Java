09dc9ed27c0c3b5ba692b47a12ba9f38f4b26233
mayuri <mayurimaheshwari760@gmail.com>
2024-12-20 17:08:57 +0530

    54321 num pattern

---
+++ patterns/ten.java
@@ -0,0 +1,18 @@
+public class ten {
+    public static void main(String[] args) {
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=4;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// for(int i=1;i<=5;i++){
+//     for(int j=1;j<=5;j++){
+//         System.out.print(j);
+//     }
+//     System.out.println();
+// }   
