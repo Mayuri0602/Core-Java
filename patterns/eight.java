98ca0c2e4f7a011548fef90c9b9323e6dd4f7a15
mayuri <mayurimaheshwari760@gmail.com>
2024-12-20 16:54:02 +0530

    number pattern

--- patterns/eight.java
+++ patterns/eight.java
@@ -1,8 +1,9 @@
+
 public class eight {
     public static void main(String[] args) {
         for(int i=1;i<=4;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(j);
+                System.out.print(i);
             }
             System.out.println();
         }   
