d6548793d28d7a2f8869cd1e5beaf86242266ac5
mayuri <mayurimaheshwari760@gmail.com>
2024-12-20 09:00:57 +0530

    covariant return type-return & no argument

---
+++ OOPs_2/Sub5.java
@@ -0,0 +1,12 @@
+class A {
+    A show(){
+        System.out.println("Welcome to India");
+        return this;
+    }
+}
+public class Sub5{
+    public static void main(String[] args) {
+        A a=new A();
+        a.show( );
+    }
+}
