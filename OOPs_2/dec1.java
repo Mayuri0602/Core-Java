552446840a4d4454e026b51e6e4950b77dd301e9
mayuri <mayurimaheshwari760@gmail.com>
2024-12-21 21:52:23 +0530

    return & argument-int datatype

---
+++ OOPs_2/dec1.java
@@ -0,0 +1,11 @@
+class A{
+    int show(int a){
+        return a;
+    }
+}
+public class dec1 {
+    public static void main(String[] args){
+      A p=new A();
+      System.out.println(p.show(56));
+    }
+}
