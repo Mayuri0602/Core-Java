9aa3775d550e15a1b15c794dee5cb416e3f51cae
mayuri <mayurimaheshwari760@gmail.com>
2024-12-21 22:23:26 +0530

    float-return & argument

---
+++ OOPs_2/dec3.java
@@ -0,0 +1,11 @@
+class Kajal{
+    float show(float c){
+        return c;
+    }
+}
+public class dec3 {
+    public static void main(String[] args) {
+        Kajal k=new Kajal();
+        System.out.println(k.show(34.56f));
+    }
+}
