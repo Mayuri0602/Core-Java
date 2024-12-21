dca8e7e252eb673c1e2dbeff6c2bb2cd4d80a8bc
mayuri <mayurimaheshwari760@gmail.com>
2024-12-21 22:12:58 +0530

    char-return & argument

---
+++ OOPs_2/dec2.java
@@ -0,0 +1,13 @@
+class M{
+    char show(char c){
+        return c;
+    }
+}
+
+public class dec2 {
+    public static void main(String[] args) {
+        M mayu=new M();
+        System.out.println(mayu.show('a' ));
+    
+}
+}
