588e279d016d0bd0ab0e5d2175c413eacca8edc1
mayuri <mayurimaheshwari760@gmail.com>
2024-12-22 09:49:49 +0530

    java program to print hello world

---
+++ OOPs_2/HelloA.java
@@ -0,0 +1,13 @@
+
+    class A{
+        void show( ){
+            System.out.println("Hello World");
+    }
+    }
+    public class HelloA{
+        public static void main(String[] args) {
+            A h=new A();
+            h.show( );
+        }
+    }
+
