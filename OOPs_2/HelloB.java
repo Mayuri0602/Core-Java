1998406d429d9f3ef6cc415864be9bdeabad90db
mayuri <mayurimaheshwari760@gmail.com>
2024-12-22 09:56:25 +0530

    more than one method in a single class

---
+++ OOPs_2/HelloB.java
@@ -0,0 +1,16 @@
+
+    class A{
+        void method1( ){
+            System.out.println("Hello Regex");
+    }
+        void method2( ){
+            System.out.println("Hello Java");
+        }
+    }
+    public class HelloB{
+        public static void main(String[] args) {
+            A h=new A();
+            h.method1( );
+            h.method2( );
+        }
+}
