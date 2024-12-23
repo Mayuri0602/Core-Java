ba09f0caeb29b18bd5df12581b6e0e59c90b0ed6
mayuri <mayurimaheshwari760@gmail.com>
2024-12-23 15:20:17 +0530

    user input of inst var through parameterised constructor and parameter name is also different

---
+++ Encapsulation/InstanceVariable2.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class A{
+    int a,b;
+    A(int x,int y){
+        a=x;
+        b=y;
+        System.out.println("sum: "+(a+b));
+    } 
+}
+class B{
+    int c,d;
+    B(int x,int y){
+        c=x;
+        d=y;
+        System.out.println("sub: "+(c-d));
+    }
+}
+class C{
+    int e,f;
+    C(int x,int y){
+        e=x;
+        f=y;
+        System.out.println("multi: "+(e*f));
+    }
+}
+class D{
+    int g,h;
+    D(int x,int y){
+        g=x;
+        h=y;
+        System.out.println("div: "+(g/h));
+    }
+}
+
+public class InstanceVariable2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of x and y: ");
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        new A(x,y);
+        new B(x,y);
+        new C(x,y);
+        new D(x,y);
+    }
+    }
+    
+
