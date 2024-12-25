d11041cc75a39511878c074c9bff72adddbde817
mayuri <mayurimaheshwari760@gmail.com>
2024-12-25 23:35:52 +0530

    Basic Constructor

---
+++ Encapsulation/Constructor1.java
@@ -0,0 +1,15 @@
+class Book{
+    String title;
+    String author;
+        Book(){
+            title="One Indian Girl";
+            author="Chetan Bhagat";
+        System.out.println("title:" +title);
+         System.out.println("author:" +author );
+    }
+}
+public class Constructor1 {
+    public static void main(String[] args) {
+        new Book(); 
+    }
+}
